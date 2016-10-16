f8715ef872380265600ffa19beba2cbdac2ac5d0
storyfree <rnnwkals1@naver.com>
2016-10-17 01:21:54 +0900

    why

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pymongo
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -24,4 +25,6 @@ def index(query):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
