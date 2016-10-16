57e632d4afd84b3aabc9358b0f601c2dfc6cc985
storyfree <rnnwkals1@naver.com>
2016-10-17 00:53:53 +0900

    for heroku

--- mongo_test.py
+++ mongo_test.py
@@ -2,8 +2,10 @@ import pymongo
 from flask import Flask, render_template
 
 app = Flask(__name__)
-client = pymongo.MongoClient("localhost", 27017)
+#client = pymongo.MongoClient("localhost", 27017)
+client = pymongo.MongoClient("mongodb://rnnwkals1:hi000319@ds041939.mlab.com:41939/ani_db")
 db = client.ani_db
+print(db.anime2.find_one({"info.name": {"$regex": "나"}}))
 def query2db(query):
     anis = db.anime2.find_one({"info.name": {"$regex": query+""}})
     # print(anis["info"]["name"])
@@ -12,13 +14,13 @@ def query2db(query):
     #     print(ani["name"], ani["url"], ani["sumnail"])
     return anis
 
-
-@app.route('/<query>')
-def index(query):
-    anis = query2db(query)
-    return render_template("ani_page.html", anis=anis)
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#
+# @app.route('/<query>')
+# def index(query):
+#     anis = query2db(query)
+#     return render_template("ani_page.html", anis=anis)
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ mongo_test2.py
@@ -0,0 +1,27 @@
+import pymongo
+from flask import Flask, render_template
+
+app = Flask(__name__)
+client = pymongo.MongoClient("mongodb://rnnwkals1:hi000319@ds041939.mlab.com:41939/ani_db")
+db = client.ani_db
+def query2db(query):
+    anis = db.anime2.find_one({"info.name": {"$regex": query+""}})
+    # print(anis["info"]["name"])
+    # print(anis["info"]["sumnail"])
+    # for ani in anis["list"]:
+    #     print(ani["name"], ani["url"], ani["sumnail"])
+    return anis
+
+@app.route('/')
+def hello():
+    return "hello world"
+
+@app.route('/<query>')
+def index(query):
+    anis = query2db(query)
+    #return render_template("ani_page.html", anis=anis)
+    return "jamin:"+query+str(anis)+"end"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
