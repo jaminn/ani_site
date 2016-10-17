3c0fdc5c4b1470dfb7c2ac4dd2cab25f3c76f89a
rnnwkals1@naver.com <tkdcjs7777@gmail.com>
2016-10-17 13:59:15 +0900

    a

--- app.py
+++ app.py
@@ -5,26 +5,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb://rnnwkals1:hi000319@ds041939.mlab.com:41939/ani_db")
 db = client.ani_db
+
+
 def query2db(query):
-    anis = db.anime2.find_one({"info.name": {"$regex": query+""}})
+    anis = db.anime2.find_one({"info.name": {"$regex": query + ""}})
     # print(anis["info"]["name"])
     # print(anis["info"]["sumnail"])
     # for ani in anis["list"]:
     #     print(ani["name"], ani["url"], ani["sumnail"])
     return anis
 
+
 @app.route('/')
 def hello():
-    return "hello world????!"
+    return "sex on the beach"
+
 
 @app.route('/<query>')
 def index(query):
     anis = query2db(query)
     return render_template("ani_page.html", anis=anis)
-    #return "jamin:"+query+str(anis)+"end"
+    # return "jamin:"+query+str(anis)+"end"
 
 
 if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
